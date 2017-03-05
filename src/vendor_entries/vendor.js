24656235742dd65a41e66ae66cac2c02c7314c48
kiparenkomb <kiparenkomb@gmail.com>
2017-03-05 21:17:33 +0200

    start new-project

---
+++ src/vendor_entries/vendor.js
@@ -0,0 +1,9 @@
+module.exports = [
+	'./bower/jquery/dist/jquery.js',
+	'./bower/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+	'./bower/modernizr/src/modernizr.js',
+	'./bower/masonry/dist/masonry.pkgd.js',
+	'./bower/imagesloaded/imagesloaded.js',
+	'./bower/classie/classie.js',
+	'./src/js/animOnScroll.js',
+];
